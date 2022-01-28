eee0e244d0a4ce33490bcbf5d1e0dfe9ffe0a76d
Danny Trunk <dtrunk90@gmail.com>
2022-01-28 15:37:29 +0100

    beryllium: Add shim for missing symbol in lib-imsvt.so
    Fixes:
    
        E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system/product/lib64/lib-imsvideocodec.so"...
    
    Change-Id: I4364a1e1da2ae2120194f44c233f1f8b1cd0f7ac

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Sourced by the common device repo when extracting device-specific blobs
+function blob_fixup() {
+    case "${1}" in
+    system_ext/lib64/lib-imsvideocodec.so)
+        ${PATCHELF} --add-needed "lib-imsvtshim.so" "${2}"
+        ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
