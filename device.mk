2203df0a99ef9dc02b822e328ed388607b109f5f
Bruno Martins <bgcngm@gmail.com>
2022-05-05 15:07:50 +0100

    beryllium: Clean up fingerprint related stuff
    The fingerprint HIDL service is now built in the common tree.
    As for the manifest entries, remove whatever is unused.
    
    Change-Id: I781f597b35a557e1beb035fae557695488fe2c22

--- device.mk
+++ device.mk
@@ -74,10 +74,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845
-
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qdcm_calib_data_ebbg_fhd_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ebbg_fhd_video_dsi_panel.xml \
