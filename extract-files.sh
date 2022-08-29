022e1fee42ebacb9a39e5afd68c0f1c8296be213
Ivan Vecera <ivan@cera.cz>
2022-08-29 16:03:41 +0200

    beryllium: redefining shim code for existing symbol in lib-imsvideocodec.so
     * Resolves:
       E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system/system_ext/lib64/lib-imsvideocodec.so"

--- extract-files.sh
+++ extract-files.sh
@@ -10,7 +10,7 @@
 function blob_fixup() {
     case "${1}" in
     system_ext/lib64/lib-imsvideocodec.so)
-        ${PATCHELF} --add-needed "lib-imsvtshim.so" "${2}"
+        "${PATCHELF}" --add-needed "lib-imsvtshim.so" "${2}"
         ;;
     esac
 }
