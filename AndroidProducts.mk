b2b7a8949b857825f85d904d8cd1a5cc7747cfc7
Shikhar <krtonia@gmail.com>
2023-01-09 00:20:40 +0530

    beryllium: Bringup for ArrowOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/arrow_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    arrow_beryllium-user \
+    arrow_beryllium-userdebug \
+    arrow_beryllium-eng
--- arrow_beryllium.mk
+++ arrow_beryllium.mk
@@ -6,11 +6,21 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+# Arrow Stuff
+ARROW_COMMUNITY := true
+ARROW_GAPPS := true
+WITH_GMS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+# BootAnimation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := arrow_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
