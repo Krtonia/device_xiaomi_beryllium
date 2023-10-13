54a533f3894eac93b32dbd597890d4a2a6c46e44
Shikhar <krtonia@gmail.com>
2023-10-13 22:17:33 +0530

    beryllium: Drop Pocket Mode

--- device.mk
+++ device.mk
@@ -88,10 +88,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
