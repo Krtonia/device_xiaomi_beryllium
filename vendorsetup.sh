d49bb0430e28f1269e098f1c725968e79ec799c3
Shikhar <krtonia@gmail.com>
2024-06-03 21:53:17 +0530

    beryllium: Add vendorsetup for blaze
    Signed-off-by: Shikhar <krtonia@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# common tree
+git clone https://github.com/Krtonia/device_xiaomi_sdm845-common.git -b blaze device_xiaomi_sdm845-common
+
+# Radioactive kernel
+git clone https://github.com/Krtonia/Radioactive_kernel.git -b 14 --depth=1 kernel/xiaomi/sdm845
+
+# vendor 
+git clone https://github.com/Krtonia/beryllium-vendor.git -b 14 vendor/xiaomi
+
+# Keys
+git clone https://github.com/afterallafk/vendor_extra.git -b 14 vendor/extra
